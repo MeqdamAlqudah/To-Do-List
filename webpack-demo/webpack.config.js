2a402954303ae85fd19b009e6510513b87ae84e7
MeqdamAlqudah <memuq56@gmail.com>
2021-12-02 21:22:14 +0200

    Try to fix linter errors

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -4,30 +4,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 /* eslint-enable */
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/index.js',
-    },
-    devServer: {
-        static: './dist',
-    },
-    devtool: 'inline-source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
+    static: './dist',
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
+  },
 };
